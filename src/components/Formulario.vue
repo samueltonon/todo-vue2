3d04862621e988f6b325e3ba9b64b4525f1e957c
Samuel Tonon <samuel_tonon@outlook.com>
2026-03-15 22:23:37 -0300

    componentizing the application

---
+++ src/components/Formulario.vue
@@ -0,0 +1,25 @@
+<script setup>
+    const props = defineProps(['cadastraTarefa', 'tarefaTemporaria', 'trocarFiltro', 'editaTarefaTemp']);
+</script>
+
+<template>
+    <form @submit.prevent="props.cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemporaria"
+                    @change="props.editaTarefaTemp" required type="text"
+                    placeholder="Digite a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                    <option value="todas">Todas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="concluidas">Concluídas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['tarefas']);
+</script>
+
+<template>
+    <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa.titulo">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" v-model="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <p v-else class="text-center mt-4 text-muted">
+        Não existem tarefas pendentes
+    </p>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
